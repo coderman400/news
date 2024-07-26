d73383ec9c01ead508d5f1acc9dd71f9daf7408d
arvind <achurox1121@gmail.com>
2024-07-26 22:04:35 +0530

    setup views and route

--- src/index.js
+++ src/index.js
@@ -2,6 +2,16 @@ const express = require('express')
 const app = express()
 const port = 5000
 
-//listen on 5000
+app.use(express.static('css'))
+app.use('/css',express.static(__dirname+'css'))
+app.use('/img',express.static(__dirname+'img'))
+
+app.set('views', './src/views')
+app.set('view engine', 'ejs')
+
+const newsRouter = require('./routes/news')
+
+app.use('/', newsRouter)
+
 app.listen(port, ()=> console.log(`listening on ${port}`))
 
