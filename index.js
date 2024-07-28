39a8da2c507a55e88a4345c1a019c3ad3e7d598e
arvind <achurox1121@gmail.com>
2024-07-28 13:28:14 +0530

    make ejs logic for newscards, add additional logic and stylesheet

--- index.js
+++ index.js
@@ -2,14 +2,16 @@ const express = require('express')
 const app = express()
 const port = 5000
 
-app.use(express.static('css'))
-app.use('/css',express.static(__dirname+'css'))
-app.use('/img',express.static(__dirname+'img'))
+// app.use(express.static('css'))
+// app.use('/css',express.static(__dirname+'css'))
+// app.use('/img',express.static(__dirname+'img'))
+
+app.use(express.static('public'))
 
 app.set('views', './src/views')
 app.set('view engine', 'ejs')
 
-const newsRouter = require('./routes/news')
+const newsRouter = require('./src/routes/news')
 
 app.use('/', newsRouter)
 
