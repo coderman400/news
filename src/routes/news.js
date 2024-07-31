da6b186c940ab88670178517f56d74749b7180e0
arvind <achurox1121@gmail.com>
2024-07-31 13:42:23 +0530

    change api, add country selections, make some ejs changes

--- src/routes/news.js
+++ src/routes/news.js
@@ -2,22 +2,35 @@ const express = require('express')
 const newsRouter = express.Router()
 const axios = require('axios')
 
-newsRouter.get('', async(req,res) => {
-    // res.render('news')
+// newsRouter.get('', async(req,res) => {
+//     // res.render('news')
+    
+//     try{
+//         const newsAPI = await axios.get(`https://newsapi.org/v2/top-headlines?country=fr&from=2024-07-30&sortBy=popularity&apiKey=${process.env.apikey}`)
+//         res.render('news', {articles: newsAPI.data.articles})
+//     }catch(err){
+//         if(err.response){
+//             console.log(err.response.data)
+//             console.log(err.response.status)
+//             console.log(err.response.header)
+//         }else if(err.request){
+//             console.log(err.request)
+//         }else{
+//             console.error('ERROR',err.message)
+//         }
+//     }
+// })
 
-    try{
-        const newsAPI = await axios.get(`https://newsapi.org/v2/top-headlines?country=jp&from=2024-07-25&sortBy=popularity&apiKey=${process.env.apikey}`)
-        res.render('news', {articles: newsAPI.data.articles})
-    }catch(err){
-        if(err.response){
-            console.log(err.response.data)
-            console.log(err.response.status)
-            console.log(err.response.header)
-        }else if(err.request){
-            console.log(err.request)
-        }else{
-            console.error('ERROR',err.message)
-        }
+
+newsRouter.get('', async (req, res) => {
+    const country = req.query.country || 'us';
+    console.log("CALLED THIS WITH VALUE: " + country)
+    try {
+        const newsAPI = await axios.get(`https://api.goperigon.com/v1/all?apiKey=${process.env.apikey}&country=${country}&from=2024-07-25&to=2024-07-30`);
+        res.render('news', { country, articles: newsAPI.data.articles });
+    } catch (err) {
+        console.error('Error fetching news:', err);
+        res.status(500).send('An error occurred while fetching news');
     }
-})
+});
 module.exports = newsRouter
